9a676ab014f4651db671b59717cb243a56c41a60
TdotTemi <Admin@Admins-MacBook.local>
2017-03-13 00:30:47 -0400

    Canadian Weather App SPA

---
+++ controllers.js
@@ -0,0 +1,50 @@
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
+    
+    //list to populate the select element
+    $scope.names = [{id:"1",name:"Calgary, Alberta", value:"Calgary,Canada"}, {id:"2",name:"Montreal, Quebec", value:"Montreal,Canada"},{id:"3",name:"Ottawa, Ontario", value:"Ottawa,Canada"}, {id:"4", name:"Toronto, Ontario", value:"Toronto,Canada"},{id:"5",name:"Winnepeg, Manitoba", value:"Winnepeg,Canada"}];
+    
+    //submission of form goes to forecast fragment hash
+    $scope.submit = function() {
+        $location.path("/forecast");
+    };
+    
+    //called when the select's option changes
+    $scope.changedValue=function(item){
+        $scope.cityName = item.value;//city field to pass to api
+        console.log($scope.cityName);
+     };
+    
+    //manual watcher for cityName
+      $scope.$watch('cityName', function() {
+       cityService.cityName = $scope.cityName; 
+          console.log(cityService.cityName);
+    });
+    
+    $scope.cityName = cityService.cityName;
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    
+    $scope.cityName = cityService.cityName;
+    
+    $scope.days = $routeParams.days || '2';
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=2fb3abccd2d5eca857f2fab50f7f0192", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.cityName, cnt: $scope.days }); //get cityName and for specified days
+    
+    $scope.convertToFahrenheit = function(degK) {
+        
+        return Math.round(degK - 273.15) + '\u00B0C'; //add degree sign and C to the temp
+        
+    }
+    
+    $scope.convertToDate = function(dt) { 
+      
+        return new Date(dt * 1000);
+        
+    };
+    
+}]);
---
+++ routes.js
@@ -0,0 +1,26 @@
+// ROUTES
+weatherApp.config(function ($routeProvider) {
+   
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/home.htm',
+        controller: 'homeController'
+    })
+    
+    .when('/about', {
+        templateUrl: 'pages/about.htm',
+        //controller: 'homeController'
+    })
+    
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
+});
